5e4f56a3bfa8cbe0e79f730cd8d142d2c2bc5084
Raghukongala <kongalaraghu406@gmail.com>
2026-06-20 22:49:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Raghu DevOps Self-Hosted Runner 🚀\n");
+  res.end("Hello from Kiran DevOps Self-Hosted Runner 🚀\n");
 });
 
 server.listen(PORT, () => {
